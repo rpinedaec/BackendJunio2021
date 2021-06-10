702d36ea4d2da0ccd6975e1e07940c01063d479e
Roberto David Pineda Lopez <rpineda@zegelipae.edu.pe>
2021-06-09 21:07:53 -0500

    rpineda: pre-comit

--- Semana2Sesion3/init.py
+++ Semana2Sesion3/init.py
@@ -1,1 +1,28 @@
+# import os
+# import sys
+# from subprocess import Popen, PIPE
+
+
 print("Hola")
+
+
+def long_function_name(
+        var_one, var_two,
+        var_three, var_four):
+    return "ciao"
+
+
+var_one = 1
+var_two = 2
+var_three = 3
+var_four = 4
+foo = long_function_name(var_one, var_two,
+                         var_three, var_four)
+
+
+print(foo)
+
+my_list = [
+    1, 2, 3,
+    4, 5, 6,
+]

86bae021ccd5ffd4840ee267ebb2173532c975d0
Roberto David Pineda Lopez <rpineda@zegelipae.edu.pe>
2021-06-09 21:13:48 -0500

    Prueba commit2

--- Semana2Sesion3/init.py
+++ Semana2Sesion3/init.py
@@ -6,9 +6,7 @@
 print("Hola")
 
 
-def long_function_name(
-        var_one, var_two,
-        var_three, var_four):
+def long_function_name(var_one, var_two, var_three, var_four):
     return "ciao"
 
 
@@ -16,13 +14,9 @@ var_one = 1
 var_two = 2
 var_three = 3
 var_four = 4
-foo = long_function_name(var_one, var_two,
-                         var_three, var_four)
+foo = long_function_name(var_one, var_two, var_three, var_four)
 
 
 print(foo)
 
-my_list = [
-    1, 2, 3,
-    4, 5, 6,
-]
+my_list = [1, 2, 3, 4, 5, 6]

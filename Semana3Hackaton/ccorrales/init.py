c24aa31910969119f0842943cd2e948f43e31028
Carlos Corrales <ca.corrales@hotmail.com>
2021-06-20 00:46:40 -0500

    cacorrales: dni

--- Semana3Hackaton/ccorrales/init.py
+++ Semana3Hackaton/ccorrales/init.py
@@ -1,22 +1,20 @@
 print ("****--VALIDAR SU NUMERO DE DNI--****")
-
 nom = input("Ingrese su Nombre :")
 ape = input("Ingrese su Apellido :")
-num = (3,2,7,6,5,4,3,2)
-#Descomponiendo el numero de DNI
 lst = []
 def string_int(my_data):
     for i in my_data:
         lst.append(int(i))    
-    #print(lst)
+    print(lst)
 while True:
     try:  
         dni = input("Ingrese el numero de DNI a validar  :")
         string_int(dni)
         break
     except ValueError:
-        print ("No es un numero valido")
+        print ("No es un numero DNI valido")
 usuario = print ("Estimado  " + nom +"  "+ ape + "  su DNI es   :", lst)
-dnivalor = lst
-dni1=lst(0)*num(0)
-print("dni1")
+def codval(x):
+    for indice in range(0,8,1):
+        print(indice)
+        

1441c6bcc362d0a31bdadd38417b0d0510810803
Roberto David Pineda Lopez <rpineda@zegelipae.edu.pe>
2021-07-10 08:18:20 -0500

    rpineda: Hackaton semana 6

---
+++ Semana6Hackaton/rpineda/models/alumno.py
@@ -0,0 +1,7 @@
+from Semana6Tutoria.rpineda.models.persona import Persona
+import persona
+
+class Alumno(Persona):
+    def __init__(self,codigo, nombre, dni, edad, correo):
+        super().__init__(nombre, dni, edad, correo)
+        self.codigo = codigo

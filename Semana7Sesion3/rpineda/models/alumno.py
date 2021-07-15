1a1939d4d3a11add511ba815b68355039ad2dacd
Roberto David Pineda Lopez <rpineda@zegelipae.edu.pe>
2021-07-14 21:23:51 -0500

    rpineda: clase semana 7 sesion 3

---
+++ Semana7Sesion3/rpineda/models/alumno.py
@@ -0,0 +1,31 @@
+import sys
+sys.path.insert(0, '..')
+from utils.utils import log
+from conn.conexion import conexionBDD
+
+class Alumno:
+    def __init__(self, nombre, notas):
+        self.nombre = nombre
+        self.notas = notas
+
+    @staticmethod
+    def ingresarAlumno(notas):
+        alumnos = []
+        thislog = log("ingresar")
+        for i in notas:
+            thislog.debug(i.notas)
+            listaNotas = i.notas
+            promedio = sum(listaNotas)/len(listaNotas)
+            insert = {
+                'alumno': i.nombre,
+                'notas' : listaNotas,
+                'notaMinima': min(listaNotas),
+                'notaMaxima': max(listaNotas),
+                'promedio':promedio
+            }
+            alumnos.append(insert)
+        if alumnos:
+            
+            conn = conexionBDD(4)
+            for item in alumnos:
+                conn.insertarRegistro("notas", item)

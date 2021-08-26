3c699096a3ea4b40cf3fd774364c05f4dd482e91
Roberto David Pineda Lopez <rpineda@zegelipae.edu.pe>
2021-08-25 20:45:16 -0500

    Clase semana 13 sesion 3

--- Semana12Sesion2/rpineda/pachaqtec/urls.py
+++ Semana12Sesion2/rpineda/pachaqtec/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('polls/', include('polls.urls')),
+    path('report/', include('report.urls')),
     path('admin/', admin.site.urls),
 ]
